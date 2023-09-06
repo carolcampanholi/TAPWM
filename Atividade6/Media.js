f8b1e41c7924cfa21f2407c463880d699fa55afa
Ana Carolina Campanholi <102394420+carolcampanholi@users.noreply.github.com>
2023-09-06 17:39:54 -0300

    Add files via upload

---
+++ Atividade6/Media.js
@@ -0,0 +1,9 @@
+var nome;
+
+var nome = prompt("Digite seu nome completo: ")
+var n1 = parseFloat (prompt("Informe a primeira nota:"))
+var n2 = parseFloat (prompt("Informe a segunda nota:"))
+var n3 = parseFloat (prompt("Informe sua terceira nota:"))
+
+var media = ((n1)+ (n2) + (n3)) /3;
+alert("Seu nome é: " + nome + ", e a média das suas notas são: " + media);
---
+++ Atividade6/Operacoes.js
@@ -0,0 +1,7 @@
+
+var num1 = parseFloat(prompt("Informe o primeiro número: "))
+var num2 = parseFloat(prompt("Informe o segundo número: "))
+
+var numeros = ((num1)+(num2)) - (num2) - (num2) / 2 (num1) % (num2)
+
+alert("O resultado é:" + numeros);
