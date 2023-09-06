3df3029eb7acba1426783407b311a15536f3a4c0
Ana Carolina Campanholi <102394420+carolcampanholi@users.noreply.github.com>
2023-09-06 17:06:22 -0300

    Add files via upload

---
+++ Atividade5/atividade5.js
@@ -0,0 +1,17 @@
+
+x = 1;
+var y = 2;
+
+function exemplo() {
+    var z = 3;
+    w = 4;
+    let i = 5;
+
+    x = x * 10;
+    y = y * 10;
+    return `x=${x} y=${y} z=${z} w=${w} i=${i}`;
+}
+
+alert(exemplo());
+alert(`x=${x} y=${y} z=${z} w=${w} i=${i}`);
+
