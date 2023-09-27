793945f31dad6223a38076c123e14b2eb1f26fd0
Ana Carolina Campanholi <102394420+carolcampanholi@users.noreply.github.com>
2023-09-27 18:11:31 -0300

    Add files via upload

---
+++ Atividade 11/funcoes.js
@@ -0,0 +1,6 @@
+var num1 = parseFloat(prompt("Informe o primeiro número: "));
+var num2 = parseFloat(prompt("Informe o segundo número: "));
+var num3 = parseFloat(prompt("Informe o terceiro número: "));
+var num4 = parseFloat(prompt("Informe o último número: "));
+
+function funcao (num1, num2, num3, num4);
